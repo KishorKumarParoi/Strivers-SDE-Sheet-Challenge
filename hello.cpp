2abac2a1871dfa8e588d691e0a3583d23fc0ad17
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2024-01-29 16:01:36 +0600

    configured settings, edited mingw file for bits/stdc++.h supporting

---
+++ hello.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int x;
+    cin >> x;
+    cout << x << endl;
+    cout << "Hello World " << x << endl;
+}
