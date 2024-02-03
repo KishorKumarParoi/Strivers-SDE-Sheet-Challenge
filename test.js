69890cebb62410e7d77366711dd6511f842de7f4
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2024-02-03 18:57:02 +0600

    added snippet-file now looking for testcases shortcut

--- test.js
+++ test.js
@@ -1,46 +1,84 @@
+/*
+Author : @Kishor_Paroi
+Github: KishorKumarParoi
+Date: 2024-02-03
+Time: 18:46:11
+Problem: Set-Matrix-Zeroes
+Link: Leetcode
+*/
 "use strict"
 process.stdin.resume()
 process.stdin.setEncoding("utf-8")
-
 const printLine = (x) => {
-    console.log(x)
+    console.log(x);
 }
-
 const print = (x) => {
-    process.stdout.write(x)
+    process.stdout.write(x);
 }
-
-let inputString = ""
-let currentLine = 0
-
+let inputString = "";
+let currentLine = 0;
 process.stdin.on("data", (inputStdin) => {
-    inputString += inputStdin
+    inputString += inputStdin;
 })
-
 process.stdin.on("end", () => {
-    inputString = inputString.split("\n")
-    main()
+    inputString = inputString.split("\n");
+    main();
 })
-
 const readline = () => {
     return inputString[currentLine++];
 }
 
+let setZeroes = function (matrix) {
+    const row = matrix.length;
+    const col = matrix[0].length;
+
+    let col0 = 1;
+    // const dummyCol = matrix[0][...];
+    // const dummyRow = matrix[...][0];
+
+    for (let i = 0; i < row; i++) {
+        for (let j = 0; j < col; j++) {
+            if (matrix[i][j] === 0) {
+                if (j === 0) {
+                    col0 = 0;
+                } else {
+                    matrix[0][j] = 0;
+                }
+
+                matrix[i][0] = 0;
+            }
+        }
+    }
+
+    for (let i = 1; i < row; i++) {
+        for (let j = 1; j < col; j++) {
+            if (matrix[0][j] === 0 || matrix[i][0] === 0) {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+
+    if (col0 === 0) {
+        for (let i = 0; i < row; i++) {
+            matrix[i][0] = 0;
+        }
+    }
+
+    if (matrix[0][0] === 0) {
+        for (let j = 0; j < col; j++) {
+            matrix[0][j] = 0;
+        }
+    }
+}
+
 const main = () => {
-    // Start your code from here
+    // Start your code from here 
     const t = readline();
     const k = readline();
     const p = readline();
-
     console.log(t);
     console.log(k);
     console.log(p);
+
+
 }
-/*
-How we get input ?
-    -> At first we need to create a txt file input.txt where we write our input
-        -> Then we need to add some code inside main function
-     const t = readline()
-console.log(t)
-    -> Finally run command ** cat input.txt | node test.js > output.txt ** in terminal
-Now you can see output inside output.txt file */
