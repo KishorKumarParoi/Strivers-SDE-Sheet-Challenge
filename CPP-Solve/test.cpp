3f78b5ad46445c5fc82deaee6a43509575c9fa99
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2024-04-30 09:12:49 +0600

    c++ settings

---
+++ CPP-Solve/test.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n; cin >> n;
+    cout << n << endl;
+    while(n--){
+        cout << n << "\n";
+    }
+    cout<<"Hello World";
+    return 0;
+}
