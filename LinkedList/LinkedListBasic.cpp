dcfa7676357581d2fc218624ad5bfd125214e1b1
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2024-06-19 10:44:10 +0600

    bit and linkedlist

---
+++ LinkedList/LinkedListBasic.cpp
@@ -0,0 +1,144 @@
+// 🕉️ Jai Siya Ram 🕉️//
+/*
+Author : @Kishor_Paroi
+Github: KishorKumarParoi
+Date: 2024-06-18
+Time: 14:13:12
+Problem: LinkedList
+Link:
+*/
+#include <bits/stdc++.h>
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimization("unroll-loops")
+
+#define endl "\n"
+#define int long long
+#define sz(s) (int)s.size()
+#define all(v)                            v.begin(),v.end()
+#define TEST                              int tc;cin>>tc; while(tc--)
+// Debug
+#define d(a)                              cout << #a << " : " << a << " ";
+#define dl(a)                             cout << #a <<" : " << a << endl;
+#define print(v)  for(auto x : v)         {cout<<x<<" ";   }  cout<<endl;
+#define PRINT(v)  for(auto x : v)         {cout<<x.first<<" "<<x.second<<endl;}
+#define printArr(a,b,arr)                 for(int i=a;i<=b;++i) { cout<<arr[i]<< " ";} cout<<endl;
+
+using namespace std;
+
+const int N = 2e5 + 5;
+const int MOD = 1e9 + 7;
+
+template<typename T>
+
+class Node {
+public:
+    T data;
+    Node<T>* next;
+
+    Node() {
+        this->data = 0;
+        this->next = NULL;
+    }
+    Node(T data1, T* next1) {
+        this->data = data1;
+        this->next = next1;
+    }
+
+    Node(T data1) {
+        this->data = data1;
+        this->next = NULL;
+    }
+};
+
+
+Node<int>* LinkedListTraversalFromArray(vector<int>& arr, int n) {
+    Node<int>* head = new Node(arr[0]);
+    Node<int>* mover = head;
+
+    for (int i = 1; i < n; i++) {
+        Node<int>* temp = new Node(arr[i]);
+        mover->next = temp;
+        mover = temp;
+    }
+    return head;
+}
+
+void printLinkedList(Node<int>* head) {
+    Node<int>* temp = head;
+    while (temp) {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+int LengthOfLinkedList(Node<int>* head) {
+    Node<int>* temp = head;
+    int length = 0;
+
+    while (temp) {
+        temp = temp->next;
+        length++;
+    }
+    return length;
+}
+
+bool searchElementInLinkedList(Node<int>* head, int target) {
+    Node<int>* temp = head;
+    int length = 0;
+
+    while (temp) {
+        if (temp->data == target) {
+            return true;
+        }
+        temp = temp->next;
+        length++;
+    }
+    return false;
+}
+
+
+Node<int>* removesHead(Node<int>* head) {
+    Node<int>* temp = head;
+    head = head->next;
+    delete temp;
+    return head;
+}
+
+Node<int>* removesTail(Node<int>* head) {
+    Node<int>* temp = head;
+    while (temp->next->next != NULL) {
+        temp = temp->next;
+    }
+    temp->next = NULL;
+
+    Node<int>* tail = temp;
+    return tail; // new tail
+}
+
+void solve() {
+    int n; cin >> n;
+    vector<int>arr(n);
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    Node<int>* head = LinkedListTraversalFromArray(arr, n);
+    cout << head->data << endl;
+    head = removesHead(head);
+    Node<int>* tail = removesTail(head);
+    cout << "tail : " << tail->data << endl;
+    printLinkedList(head);
+    cout << LengthOfLinkedList(head) << endl;
+    // cout << searchElementInLinkedList(head, 1) << endl;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(!cin.tie(nullptr));
+
+    // TEST
+    solve();
+
+    return 0;
+}
