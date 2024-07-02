de68a543c8e585eb0f630b7de0460291cd1be129
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2024-07-02 19:51:27 +0600

    Count inversion in O(nlogn)

--- Array/Day-04-Array-Part-IV/Count-Inversion.cpp
+++ Array/Day-04-Array-Part-IV/Count-Inversion.cpp
@@ -1,14 +1,20 @@
-// Let's Begin Mara Khawa ^+^
-// author : @I_Love_My_Sherniii
-
-// 20-06-23
+// 🕉️ Jai Siya Ram 🕉️//
+/*
+Author : @Kishor_Paroi
+Github: KishorKumarParoi
+Date: 2024-07-02
+Time: 13:58:23
+Problem: Count Inversion
+Link:
+*/
 #include <bits/stdc++.h>
-            
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimization("unroll-loops")
+
 #define endl "\n"
 #define int long long
 #define sz(s) (int)s.size()
-#define pi acos(-1.0)
-#define fr(i,a,b)                         for(int i=a;i<=b;++i)
 #define all(v)                            v.begin(),v.end()
 #define TEST                              int tc;cin>>tc; while(tc--)
 // Debug
@@ -20,16 +26,92 @@
 
 using namespace std;
 
-const int N   = 1e6 + 5;
+const int N = 2e5 + 5;
 const int MOD = 1e9 + 7;
 
-void solve(){
-  
+int merge(vector<int>& arr, int low, int mid, int high) {
+  vector<int>temp;
+  int left = low, right = mid + 1;
+  int cnt = 0;
+
+  while (left <= mid and right <= high) {
+    if (arr[left] <= arr[right]) {
+      temp.push_back(arr[left]);
+      left++;
+    }
+    else {
+      temp.push_back(arr[right]);
+      right++;
+      cnt += (mid - left + 1);
+    }
+  }
+
+  while (left <= mid) {
+    temp.push_back(arr[left]);
+    left++;
+  }
+
+  while (right <= high) {
+    temp.push_back(arr[right]);
+    right++;
+  }
+
+  for (int i = low; i <= high; i++) {
+    arr[i] = temp[i - low];
+  }
+
+  return cnt;
 }
 
-int32_t main(){
+// The number of local inversions is the number of indices i where:
+// 0 <= i < n - 1
+// nums[i] > nums[i + 1]
+
+int countLocalInversion(vector<int>& arr) {
+  int n = arr.size();
+  int cnt = 0;
+  for (int i = 0; i < n - 1; i++) {
+    if (arr[i] > arr[i + 1])
+      cnt++;
+  }
+  return cnt;
+}
+
+int mergeSort(vector<int>& arr, int low, int high) {
+  if (low >= high)
+    return 0;
+  int cnt = 0;
+  int mid = (low + high) / 2;
+  cnt += mergeSort(arr, low, mid);
+  cnt += mergeSort(arr, mid + 1, high);
+  cnt += merge(arr, low, mid, high);
+  return cnt;
+}
+
+int countInversion(vector<int>& arr) {
+  int n = arr.size();
+  int cnt = mergeSort(arr, 0, n - 1);
+  print(arr);
+  cout << cnt << endl;
+  return cnt;
+}
+
+void solve() {
+  int n; cin >> n;
+  vector<int>arr(n);
+
+  for (int i = 0;i < n; i++) {
+    cin >> arr[i];
+  }
+
+  bool isIdealPermutation = (countLocalInversion(arr) == countInversion(arr));
+  cout << (isIdealPermutation ? "true" : "false") << endl;
+}
+
+int32_t main() {
   ios_base::sync_with_stdio(!cin.tie(nullptr));
-  
+
+  // TEST
   solve();
 
   return 0;
